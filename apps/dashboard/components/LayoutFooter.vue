b3b88fc8aad3826da8290c74dd95e46f2232681f
Linh Pham <linhpl@mainjs.net>
2018-04-11 22:02:25 +0700

    change tag a to nuxt-link in header and footer

--- apps/dashboard/components/LayoutFooter.vue
+++ apps/dashboard/components/LayoutFooter.vue
@@ -4,7 +4,7 @@
           <div class="pull-right hidden-xs">
               <b>Version</b> 2.4.0
           </div>
-          <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+          <strong>Copyright &copy; 2014-2016 <nuxt-link to="https://adminlte.io">Almsaeed Studio</nuxt-link>.</strong> All rights
           reserved.
       </footer>
 
@@ -12,8 +12,8 @@
       <aside class="control-sidebar control-sidebar-dark">
           <!-- Create the tabs -->
           <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-              <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-              <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+              <li><nuxt-link to="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></nuxt-link></li>
+              <li><nuxt-link to="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></nuxt-link></li>
           </ul>
           <!-- Tab panes -->
           <div class="tab-content">
@@ -22,7 +22,7 @@
                   <h3 class="control-sidebar-heading">Recent Activity</h3>
                   <ul class="control-sidebar-menu">
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <i class="menu-icon fa fa-birthday-cake bg-red"></i>
 
                               <div class="menu-info">
@@ -30,10 +30,10 @@
 
                                   <p>Will be 23 on April 24th</p>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <i class="menu-icon fa fa-user bg-yellow"></i>
 
                               <div class="menu-info">
@@ -41,10 +41,10 @@
 
                                   <p>New phone +1(800)555-1234</p>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
 
                               <div class="menu-info">
@@ -52,10 +52,10 @@
 
                                   <p>nora@example.com</p>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <i class="menu-icon fa fa-file-code-o bg-green"></i>
 
                               <div class="menu-info">
@@ -63,7 +63,7 @@
 
                                   <p>Execution time 5 seconds</p>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                   </ul>
                   <!-- /.control-sidebar-menu -->
@@ -71,7 +71,7 @@
                   <h3 class="control-sidebar-heading">Tasks Progress</h3>
                   <ul class="control-sidebar-menu">
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <h4 class="control-sidebar-subheading">
                                   Custom Template Design
                                   <span class="label label-danger pull-right">70%</span>
@@ -80,10 +80,10 @@
                               <div class="progress progress-xxs">
                                   <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <h4 class="control-sidebar-subheading">
                                   Update Resume
                                   <span class="label label-success pull-right">95%</span>
@@ -92,10 +92,10 @@
                               <div class="progress progress-xxs">
                                   <div class="progress-bar progress-bar-success" style="width: 95%"></div>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <h4 class="control-sidebar-subheading">
                                   Laravel Integration
                                   <span class="label label-warning pull-right">50%</span>
@@ -104,10 +104,10 @@
                               <div class="progress progress-xxs">
                                   <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                       <li>
-                          <a href="#javascript:void(0)">
+                          <nuxt-link to="#javascript:void(0)">
                               <h4 class="control-sidebar-subheading">
                                   Back End Framework
                                   <span class="label label-primary pull-right">68%</span>
@@ -116,7 +116,7 @@
                               <div class="progress progress-xxs">
                                   <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
                               </div>
-                          </a>
+                          </nuxt-link>
                       </li>
                   </ul>
                   <!-- /.control-sidebar-menu -->
@@ -188,8 +188,8 @@
                       <div class="form-group">
                           <label class="control-sidebar-subheading">
                               Delete chat history
-                              <a href="#javascript:void(0)" class="text-red pull-right"><i
-                                      class="fa fa-trash-o"></i></a>
+                              <nuxt-link to="#javascript:void(0)" class="text-red pull-right"><i
+                                      class="fa fa-trash-o"></i></nuxt-link>
                           </label>
                       </div>
                       <!-- /.form-group -->
