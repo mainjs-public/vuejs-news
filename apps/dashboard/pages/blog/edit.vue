abac63c03f4566669238886ff4c1720e4b169647
Linh Pham <linhpl@mainjs.net>
2018-04-12 16:25:15 +0700

    action in blog with fake data

--- apps/dashboard/pages/blog/edit.vue
+++ apps/dashboard/pages/blog/edit.vue
@@ -1,35 +1,61 @@
 <template>
     <div>
         {{data}}
-        <form-blog :data="data" :onClick="onClick"/>
+        <form-blog :data="data" :onClick="editblog"/>
     </div>
 </template>
 
 <script>
+  import find from 'lodash/find';
+  import join from 'lodash/join';
   import FormBlog from '~/components/FormBlog.vue';
+  import { mapActions } from 'vuex';
   const initData = {
     name: '',
     slug: '',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtdYe3FTd_dtcmwRwTZ03rhLACVSDYYTxpRLvm7yP3Rhry0IvD',
     description: '',
     content: '',
-    tags: ['Tech', 'Login', 'Value'],
+    tags: [],
+    string_tags: '',
     status: true,
+    category: ''
   };
   export default {
+    asyncData(context, callback) {
+      if (context.route.query.id) {
+        callback(null, {idBlog: context.route.query.id});
+      } else {
+        callback(null, {idBlog: ''});
+      }
+    },
+    computed: {
+      blogs: function () {
+        return this.$store.state.blogs
+      }
+    },
     data() {
       return {
         data: initData,
+      }
+    },
+    mounted() {
+      const existBlog = find(this.blogs, o => { return o.id === this.idBlog });
+      if (existBlog) {
+        this.data = {...existBlog, string_tags: join(existBlog.tags, ',')};
       }
     },
     components:{
       FormBlog
     },
     methods: {
+      ...mapActions([
+        'editblog',
+      ]),
       onClick(e) {
         console.log('success click blog');
         e.preventDefault();
-      }
+      },
     }
   }
 </script>
--- apps/dashboard/store/index.js
+++ apps/dashboard/store/index.js
@@ -1,15 +1,65 @@
 import gql from 'graphql-tag';
+import axios from 'axios';
+import uuid from 'uuid';
+import remove from 'lodash/remove';
+import omit from 'lodash/omit';
+import split from 'lodash/split';
 export function state () {
   return {
     data: [],
+    blogs: [],
   }
 }
 export const mutations = {
   setdata(state, data) {
     state.data = data;
   },
+  setblog(state, blogs) {
+    state.blogs = blogs;
+  },
+  addblog(state, blog) {
+    state.blogs = [ ...state.blogs, blog ];
+  },
+  editblog(state, blog) {
+    state.blogs = state.blogs.map(o => {
+      if (o.id = blog.id) {
+        return {
+          ...blog,
+          updated: new Date(),
+        }
+      } else {
+        return o;
+      }
+    });
+  },
+  deleteblog(state, id) {
+    state.blogs = remove(state.blogs, o => { return o.id !== id});
+  }
 };
 export const actions =  {
+  async editblog({ commit }, blog) {
+    if ( blog.id) {
+      // const value = {...blog, tags: split(blog.string_tags, '')}
+      await axios.patch(`https://todos-cuvsmolowg.now.sh/todos/${blog.id}`, omit(blog, ['id']));
+      commit('editblog', blog);
+    } else {
+      const value = {
+        ...blog,
+        id: uuid(),
+        comment: [],
+        created: new Date(),
+        updated: new Date(),
+        viewed: 0,
+        tags: split(blog.string_tags, ','),
+      };
+      const res = await axios.post(`https://todos-cuvsmolowg.now.sh/todos`, value);
+      commit('addblog', res.data);
+    }
+  },
+  async deleteblog({ commit }, id) {
+    await axios.delete(`https://todos-cuvsmolowg.now.sh/todos/${id}`);
+    commit('deleteblog', id);
+  },
   async nuxtServerInit ({ commit }, context) {
     // const postsQuery = gql`
     //   query {
@@ -31,6 +81,7 @@ export const actions =  {
     // .then(data => {
     //   commit('setCategories', data.categories);
     // });
-
-  }
+    const res = await axios.get('https://todos-cuvsmolowg.now.sh/todos');
+    commit('setblog', res.data)
+  },
 }
