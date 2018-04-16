dd4ac09fe95844534187779fb1c7a1b0f9d1cae2
Linh Pham <linhpl@mainjs.net>
2018-04-16 21:24:30 +0700

    page blog

--- apps/dashboard/pages/blog/index.vue
+++ apps/dashboard/pages/blog/index.vue
@@ -3,7 +3,6 @@
         <section class="content-header">
             <h1>
                 Blogs
-                <!--<small>advanced tables</small>-->
             </h1>
             <ol class="breadcrumb">
                 <li><nuxt-link to="/"><i class="fa fa-dashboard"></i> Home</nuxt-link></li>
@@ -12,59 +11,6 @@
         </section>
         <section class="content">
             <div class="row">
-                <!--<div class="col-xs-12">-->
-                    <!--<div class="box">-->
-                        <!--<div class="box-header">-->
-                            <!--<div style="display: flex; flex-direction: row; justify-content: space-between; align-items: center;">-->
-                                <!--<h3 class="box-title">Blogs List</h3>-->
-                                <!--<nuxt-link to="/blog/edit" class="btn btn-primary">-->
-                                    <!--<div style="display: flex; flex-direction: row; align-items: center;">-->
-                                        <!--<span>New blogs</span>-->
-                                    <!--</div>-->
-                                <!--</nuxt-link>-->
-                            <!--</div>-->
-                        <!--</div>-->
-                        <!--&lt;!&ndash; /.box-header &ndash;&gt;-->
-                        <!--<div v-if="$apollo.loading">Loading...</div>-->
-                        <!--<div v-else class="box-body">-->
-                            <!--<table id="example1" class="table table-bordered table-striped">-->
-                                <!--<thead>-->
-                                <!--<tr>-->
-                                    <!--<th>Image</th>-->
-                                    <!--<th>Name</th>-->
-                                    <!--<th>Slug</th>-->
-                                    <!--<th>Created</th>-->
-                                    <!--<th>Updated</th>-->
-                                    <!--<th>Status</th>-->
-                                    <!--<th>Actions</th>-->
-                                <!--</tr>-->
-                                <!--</thead>-->
-                                <!--<tbody v-if="blogs.length > 0">-->
-                                <!--<tr v-for="blog of blogs" v-bind:key="blog.id">-->
-                                    <!--<td><img class="img-circle img-sm" :src="blog.image"/></td>-->
-                                    <!--<td>{{blog.name}}</td>-->
-                                    <!--<td>{{blog.slug}}</td>-->
-                                    <!--<td>{{blog.created}}</td>-->
-                                    <!--<td>{{blog.updated}}</td>-->
-                                    <!--<td>{{blog.status}}</td>-->
-                                    <!--<td>-->
-                                        <!--<nuxt-link :to="`/blog/edit?id=${blog.id}`" class="btn btn-primary btn-xs" style="margin-right: 10px">Edit</nuxt-link>-->
-                                        <!--<button class="btn btn-danger btn-xs" @click="deleteClick($event, blog.id)">-->
-                                            <!--Delete-->
-                                        <!--</button>-->
-                                    <!--</td>-->
-                                <!--</tr>-->
-                                <!--</tbody>-->
-                                <!--<tbody v-else>-->
-                                    <!--<tr>-->
-                                        <!--<td colspan="7" style="text-align: center">Blog Empty</td>-->
-                                    <!--</tr>-->
-                                <!--</tbody>-->
-                            <!--</table>-->
-                        <!--</div>-->
-                        <!--&lt;!&ndash; /.box-body &ndash;&gt;-->
-                    <!--</div>-->
-                <!--</div>-->
                 <div class="col-xs-12">
                     <div class="box">
                         <div class="box-header">
@@ -78,36 +24,9 @@
                             </div>
                         </div>
                         <!-- /.box-header -->
-                        <div class="box-body">
-                            <table id="example1" class="table table-bordered table-striped">
-                                <thead>
-                                <tr>
-                                    <th>Image</th>
-                                    <th>Name</th>
-                                    <th>Slug</th>
-                                    <th>Created</th>
-                                    <th>Updated</th>
-                                    <th>Status</th>
-                                    <th>Actions</th>
-                                </tr>
-                                </thead>
-                                <tbody v-if="blogs.length > 0">
-                                    <tr v-for="blog of blogs" v-bind:key="blog.id">
-                                        <td><img class="img-circle img-sm" :src="blog.image"/></td>
-                                        <td>{{blog.name}}</td>
-                                        <td>{{blog.slug}}</td>
-                                        <td>{{blog.created}}</td>
-                                        <td>{{blog.updated}}</td>
-                                        <td>{{blog.status}}</td>
-                                        <td>
-                                            <nuxt-link :to="`/blog/edit?id=${blog.id}`" class="btn btn-primary btn-xs" style="margin-right: 10px">Edit</nuxt-link>
-                                            <button class="btn btn-danger btn-xs" @click="deleteClick($event, blog.id)">
-                                            Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                        <div v-if="$apollo.loading"> ...loading</div>
+                        <div class="box-body" v-else>
+                             <table-blog :blogs="blogs" :deleteClick="deleteClick"/>
                         </div>
                         <!-- /.box-body -->
                     </div>
@@ -121,6 +40,7 @@
 <script>
   import { mapActions } from 'vuex';
   import { query } from '~/apollo/queries/blog.js';
+  import TableBlog from '~/components/TableBlog.vue';
   export default {
     data () {
       return {
@@ -142,6 +62,9 @@
         e.preventDefault();
       },
     },
+    components: {
+      TableBlog
+    }
   }
 </script>
 
