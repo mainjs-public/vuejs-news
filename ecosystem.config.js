eff1b6f1e68d3d1ea4a4a4e1bb2c1536523a6d07
Dang Tien Ngoc <dangtienngoc@hotmail.com>
2018-05-03 17:16:36 +0700

    Config build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,21 @@
 module.exports = {
     apps: [{
-        name: 'manager',
+        name: 'storefront',
         script: 'npm',
-        args: "dev"
+        args: "storefront:start"
+    }, {
+        name: 'dashboard',
+        script: 'npm',
+        args: "dashboard:start"
     }],
     deploy: {
         production: {
-            key: '/Users/dangtienngoc/ssh/new24.pem',
             user: 'root',
-            host: ['62.75.244.96'],
+            host: ['104.236.119.26'],
             ref: 'origin/master',
             repo: 'git@github.com:dangtienngoc/new24-frontend.git',
-            path: '/var/www/html/new24',
-            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+            path: '/var/www/nodejs/new24',
+            'post-deploy': 'npm install && npm run storefront:build && npm run dashboard:build && pm2 startOrRestart ecosystem.config.js'
         }
     }
 }
