0656b9dd245f714fe2d32a88ee2591aca00dff87
Linh Pham <linhpl@mainjs.net>
2018-05-06 15:49:11 +0700

    show category slug in home

--- apps/home/pages/category/_slug.vue
+++ apps/home/pages/category/_slug.vue
@@ -1,11 +1,11 @@
 <template>
-    <div v-if="error.message">
-        {{error.message}}
+    <div v-if="categorySlug === null">
+        Fail slug
     </div>
     <div v-else>
         <div class="inner-page-header">
             <div class="banner">
-                <img :src="`${apiUrl}${category.image}`" alt="Banner" style="width: 100%; max-height: 401px">
+                <img :src="`${apiUrl}${categorySlug.category.image}`" alt="Banner" style="width: 100%; max-height: 401px">
             </div>
             <div class="banner-text">
                 <div class="container">
@@ -17,10 +17,10 @@
                                 </ul>
                             </div>
                             <div class="header-page-title">
-                                <h1>{{category.name}}</h1>
+                                <h1>{{categorySlug.category.name}}</h1>
                             </div>
                             <div class="header-page-subtitle">
-                                <p>{{category.description}}</p>
+                                <p>{{categorySlug.category.description}}</p>
                             </div>
                         </div>
                     </div>
@@ -33,10 +33,10 @@
         <div class="blog-page-area gallery-page category-page">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-                        <div v-if="$apollo.loading">loading...</div>
-                        <div class="row" v-else>
-                            <ul v-if="blogPagination.data.length > 0">
+                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12" v-if="$apollo.loading">loading...</div>
+                    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12" v-else>
+                        <div class="row">
+                            <ul v-if="categorySlug.data.length > 0">
                                 <li>
                                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                         <div id="news-Carousel1" class="carousel carousel-top-category slide" data-ride="carousel">
@@ -51,11 +51,11 @@
                                                 </a>
                                             </div>
                                             <div class="carousel-inner">
-                                                <div v-for="(blog, index) in blogPagination.data" :class="{item: true, active: index === 0}">
+                                                <div v-for="(blog, index) in categorySlug.data" :class="{item: true, active: index === 0}">
                                                     <div class="blog-image">
                                                         <nuxt-link :to="`/blog/${blog.slug}`">
                                                             <i class="fa fa-link" aria-hidden="true"></i>
-                                                            <img :src="`${apiUrl}${blog.image}`" :alt="blog.name" style="height: 470px">
+                                                            <img :src="`${apiUrl}${blog.image}`" :alt="blog.name" style=", width: 100%, max-height: 470px">
                                                         </nuxt-link>
                                                     </div>
                                                     <div class="dsc">
@@ -70,13 +70,9 @@
                                 </li>
                             </ul>
                         </div>
-                        <div class="row" v-if="$apollo.loading">loading...</div>
-                        <div class="row" v-else>
-                            <ul v-if="blogPagination.data.length < 1">
-                                <li>Empty blog</li>
-                            </ul>
-                            <ul v-else>
-                                <li v-for="blog of blogPagination.data" v-bind:key="blog.id">
+                        <div class="row">
+                            <ul>
+                                <li v-for="blog of categorySlug.data" v-bind:key="blog.id">
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                         <div class="carousel-inner">
                                             <div class="blog-image">
@@ -95,7 +91,7 @@
                                 </li>
                             </ul>
                         </div>
-                        <pagination :length="length" :hasNextPage="blogPagination.hasNextPage" :count="blogPagination.count" :start="start" :changeStartPagination="changeStartPagination"/>
+                        <pagination :length="length" :hasNextPage="categorySlug.hasNextPage" :count="categorySlug.count" :start="start" :changeStartPagination="changeStartPagination"/>
                     </div>
                     <content-right/>
                 </div>
@@ -111,25 +107,12 @@
   import Pagination from '~/components/Pagination.vue';
   import ContentRight from '~/components/ContentRight.vue';
   export default {
-    async asyncData ({params, app}, callback) {
-      try {
-        const slug = params.slug ? params.slug : '';
-        const client = app.apolloProvider.defaultClient;
-        const dataCategory = await client.query({query: getCategory, variables: {slug : slug}});
-        if (dataCategory.data.categorySlug === null) {
-          callback(null, {category: {}, error: {message: 'Not exist category'}});
-        } else {
-          callback(null, {category: dataCategory.data.categorySlug, error: {}});
-        }
-      } catch(error) {
-        callback(null, {category: {}, error: error});f
-      }
-    },
     data() {
       return {
         start: 0,
         length: 3,
-        blogPagination: {
+        categorySlug: {
+          category: {},
           data: [],
           count: 0,
           hasNextPage: false,
@@ -139,10 +122,11 @@
       }
     },
     apollo: {
-      blogPagination: {
-        query: queryPagination,
+      categorySlug: {
+        query: getCategory,
         variables() {
           return {
+            slug: this.$route.params.slug ? this.$route.params.slug : '',
             start: this.start * this.length,
             length: this.length
           }
