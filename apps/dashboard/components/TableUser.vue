85099fa122af752b9b4c5ef4e603b066e7afef1f
Linh Pham <linhpl@mainjs.net>
2018-05-16 16:34:08 +0700

    add modal when clear a user

--- apps/dashboard/components/TableUser.vue
+++ apps/dashboard/components/TableUser.vue
@@ -13,28 +13,46 @@
             <tr v-if="users.length < 1">
                 <td colspan="7">User empty</td>
             </tr>
-            <tr v-else v-for="user of users" v-bind:key="users.id">
+            <tr v-else v-for="user of users" v-bind:key="user.id">
                 <td>{{user.name}}</td>
                 <td>{{user.email}}</td>
                 <td>{{user.role}}</td>
                 <td>
                     <nuxt-link :to="`/user/edit?id=${user.id}`" class="btn btn-primary btn-xs" style="margin-right: 10px">Edit</nuxt-link>
-                    <button class="btn btn-danger btn-xs" @click="deleteClick($event, user.id)">Delete</button>
+                    <button class="btn btn-danger btn-xs" @click="getDataDelete(user.id, user.name)" data-toggle="modal" data-target="#modal-default">
+                        Delete
+                    </button>
                 </td>
             </tr>
             </tbody>
         </table>
+        <modal :title="`Are you sure to delete blog: ${name}?`" buttonOk="OK" :okClick="okClick"/>
     </div>
 </template>
 
 <script>
   import { API_URL } from 'shared/api';
+  import Modal from './Modal.vue';
   export default {
     props: ['users', 'deleteClick'],
     data() {
       return {
-        apiUrl: API_URL
+        id: '',
+        name: '',
+        apiUrl: API_URL,
       }
+    },
+    methods: {
+      getDataDelete(id, name) {
+        this.id = id;
+        this.name = name;
+      },
+      okClick() {
+        this.deleteClick(this.id);
+      }
+    },
+    components: {
+      Modal
     }
   }
 </script>
